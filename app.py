a9e8790efb66e4aa385cc41b2dd19a29fcefe61a
khadak bahadur shahi <khadakshahi@khadaks-MacBook-Air.local>
2025-03-05 01:13:01 +0545

    new app.ru

--- app.py
+++ app.py
@@ -193,8 +193,16 @@ def detect_document_language():
     else:
         return jsonify({'error': 'Language detection failed'}), 500
 
+'''if __name__ == '__main__':
+    os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+    app.run(host='0.0.0.0', port=5000)'''
+
+
+import os
+
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT env variable
+    app.run(host='0.0.0.0', port=port)
 
     
