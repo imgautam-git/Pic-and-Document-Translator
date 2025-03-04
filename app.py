f7f6dd0978d11245f5ea2bc7ea0ac951b4c519a4
khadak bahadur shahi <khadakshahi@khadaks-MacBook-Air.local>
2025-03-05 00:53:35 +0545

    new commit

--- app.py
+++ app.py
@@ -195,4 +195,6 @@ def detect_document_language():
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
+
+    
